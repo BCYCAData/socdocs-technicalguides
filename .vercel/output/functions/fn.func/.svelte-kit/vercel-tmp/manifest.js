31af98544000026f6152376f97450cc8720c3928
Alan Keown <AlanKeown@southernphone.com.au>
2023-06-30 11:52:13 +1000

    Initial commit

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["SOCLogo.png","SOCLogo_lite.png","SOCLogo_quarter.png","ag.png","favicon-32x32.png","nswg.jpg"]),
 	mimeTypes: {".png":"image/png",".jpg":"image/jpeg"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.74b217b5.js","app":"_app/immutable/entry/app.35f09690.js","imports":["_app/immutable/entry/start.74b217b5.js","_app/immutable/chunks/scheduler.4fc6ff3d.js","_app/immutable/chunks/singletons.363e91f9.js","_app/immutable/entry/app.35f09690.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/scheduler.4fc6ff3d.js","_app/immutable/chunks/index.2aec9f2a.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.3499a186.js","app":"_app/immutable/entry/app.6a88e1f7.js","imports":["_app/immutable/entry/start.3499a186.js","_app/immutable/chunks/scheduler.4fc6ff3d.js","_app/immutable/chunks/singletons.15dc1739.js","_app/immutable/entry/app.6a88e1f7.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/scheduler.4fc6ff3d.js","_app/immutable/chunks/index.2aec9f2a.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
